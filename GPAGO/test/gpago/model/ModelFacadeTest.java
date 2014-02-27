32cce976441954bdff08f52042ae373b15f9387c
jeffwoodard <jefflwoodard@gmail.com>
2014-02-27 17:40:06 -0500

    Commit transaction and close entityManager on each request.

--- GPAGO/test/gpago/model/ModelFacadeTest.java
+++ GPAGO/test/gpago/model/ModelFacadeTest.java
@@ -22,27 +22,25 @@ public class ModelFacadeTest {
 		int greyhoundCountBeforeTest = facade.getAllGreyhounds().size();
 		
 		// Create a new greyhound instance and persist it.
-		Greyhound g = new Greyhound();
-		g.setName("Test Greyhound");
-		facade.persistEntity(g);
-		facade.commitTransaction();
+		Greyhound greyhound = new Greyhound();
+		greyhound.setName("Test Greyhound");
+		facade.saveGreyhound(greyhound);
 		
 		List<Greyhound> greyhounds = facade.getAllGreyhounds();
 		
 		Assert.assertEquals("The expected number of greyhounds were not returned.", 1 + greyhoundCountBeforeTest, greyhounds.size());
 		
-		Greyhound greyhound = facade.getGreyhound(g.getId());
+		greyhound = facade.getGreyhound(greyhound.getId());
 		
 		Assert.assertNotNull("The test greyhound was not found in the database.", greyhounds);
 		Assert.assertEquals("The test greyhound was not found in the database.", "Test Greyhound", greyhound.getName());
 		
 		facade.removeGreyhound(greyhound);
-		facade.commitTransaction();
 		
 		greyhounds = facade.getAllGreyhounds();
 		
 		Assert.assertEquals("The test greyhound was not removed from the database.", greyhoundCountBeforeTest, greyhounds.size());
-		greyhound = facade.getGreyhound(g.getId());
+		greyhound = facade.getGreyhound(greyhound.getId());
 		Assert.assertNull("The test greyhound was not removed from the database.", greyhound);
 	}
 }
